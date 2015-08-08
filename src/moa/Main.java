308f212be9e38a15b7d8ab6f107cbf199ad28c4c
Wilde <gutopicciani@yahoo.com.br>
2015-08-08 18:17:55 -0300

    Teste

--- src/moa/Main.java
+++ src/moa/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-
+//teste
     public static void main(String[] args) {
         List<Integer> num = InputManager.ConsoleRead();
         List<Piece> pieces = new ArrayList<>();
